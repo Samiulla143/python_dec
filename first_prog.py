0d5578d5d855659c068b02204bfce87288fa9c98
md sami <msami2241@gmail.com>
2024-12-11 12:12:58 +0530

    Update first_prog.py

--- first_prog.py
+++ first_prog.py
@@ -1,8 +1,8 @@
-num = int (input('enter the amont:'))
+# num = int (input('enter the amont:'))
 
-tax= num*(18/100)
+# tax= num*(18/100)
 
-print('tax amount',tax)
+# print('tax amount',tax)
 
 
 
@@ -11,4 +11,23 @@ print('tax amount',tax)
 ip1 = int(input('enter one num:'))
 ip2 = int(input('enter second num:'))
 
+add = ip1+ip2
+sub = ip1-ip2
+mul = ip1*ip2
+div = ip1/ip2
 
+operation = input('what do you wnat to calculate:'
+                  'add'
+                  'sub'
+                  'mul'
+                  'div')
+
+
+if operation == add:
+    print(add)
+elif operation == sub:
+    print(sub)
+elif operation == mul:
+    print(mul)
+elif operation == div:
+    print(div)
