3548b980e2c131ab25d3433b36f0b9e3da08c536
md sami <msami2241@gmail.com>
2024-12-13 09:16:35 +0530

    Update 13Dec.py

--- 13Dec.py
+++ 13Dec.py
@@ -1,2 +1,24 @@
-for x in '12345':
-    print(x)
+# for x in '12345':
+#     print(x)
+
+#range(stop)
+# for i in range(3):
+#     print(i)
+    
+#range(start,stop)
+
+# for j in range(10,30):
+    # print(j)
+    
+#range(start,stop,step)
+# for k in range(1,10,3):
+#     print(k)
+
+
+#extracting  odd and even number
+
+for x in range(10):
+    if x%2==0:
+        print(x,'even number')
+    else:
+        print(x,'odd')
