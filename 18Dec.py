4a044da4cc045fc062bb9ed1e6dd38e05a4c9c40
md sami <msami2241@gmail.com>
2024-12-18 09:00:39 +0530

    Update 18Dec.py

--- 18Dec.py
+++ 18Dec.py
@@ -14,9 +14,22 @@
 #     count=count+1
 #     print(count)
     
+# count = 0
+# for x in range(1,10,2):
+#     print(x)
+#     count=count+1
+# print('total values are:',count)
+
+#to print with sum and product of odd values
 count = 0
+sum=0
+product=1
 for x in range(1,10,2):
     print(x)
     count=count+1
+    sum=sum+x
+    product=product*x
 print('total values are:',count)
+print('total sum of odd values are:',sum)
+print('total product of odd values are:',product)
     
