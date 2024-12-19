884cde5ed530ba0ae9d160f037bfb9a02a31d39c
md sami <msami2241@gmail.com>
2024-12-19 08:05:41 +0530

    Update 18Dec.py

--- 18Dec.py
+++ 18Dec.py
@@ -21,15 +21,32 @@
 # print('total values are:',count)
 
 #to print with sum and product of odd values
-count = 0
-sum=0
-product=1
-for x in range(1,10,2):
-    print(x)
-    count=count+1
-    sum=sum+x
-    product=product*x
-print('total values are:',count)
-print('total sum of odd values are:',sum)
-print('total product of odd values are:',product)
-    
+
+# count = 0
+# sum=0
+# product=1
+# for x in range(1,10,2):
+#     print(x)
+#     count=count+1
+#     sum=sum+x
+#     product=product*x
+# print('total values are:',count)
+# print('total sum of odd values are:',sum)
+# print('total product of odd values are:',product)
+
+# print("Hello, " + "World!")
+# print("Python", "is", "fun", sep="-")
+# x = 10;
+# print(x)
+
+# x = 3; y = x; x = 10; print(y)
+# name = "Alice"; NAME = "Bob"; print(name, NAME)
+# my_variable = 10
+# MYVARIABLE =11
+# my-variable=1
+# MyVariable=2
+print(10 // 3)
+print((2 + 3) * 4)
+print(-7 // 2)
+print(2 ** 3 ** 2)
+print(15 % 4)
