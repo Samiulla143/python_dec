e077afd39a782664c98029711b90deb82eb0eaf5
md sami <msami2241@gmail.com>
2024-12-19 08:59:20 +0530

    Create 19Dec.py

---
+++ 19Dec.py
@@ -0,0 +1,21 @@
+# >>>>>>>>>>>>>>>>>>>>>> Indexing <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+# val = 'rama'
+# print(val[2])
+
+
+#>>>>>>>>>>>>>>>>>>>>>>>>> slicing <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+#ref='-9-8-7-6-5-4-3-2-1
+# name='samiullah'
+#ref='012345678'
+# print(name[0:4])       #start,stop
+# print(name[-5:-1])     #start,stop
+# print(name[1:9:2])     #start,stop,step
+# print(name[::-1])      #starts with right
+
+val ='nitin'
+if val[::1] == val[::-1]:
+    print('palindrome')
+else:
+    print('not palindrome')
