180ca33e652aceecee4bed20a40c38c49404f427
md sami <msami2241@gmail.com>
2024-12-23 16:58:02 +0530

    Update 20Dec.py

--- 20Dec.py
+++ 20Dec.py
@@ -14,18 +14,18 @@
 # print(97,'chr-->',chr(97))
 
 
-# val= 'ABC'
-# for x in val:
-#     ord_val=ord(x)
-#     conv_chr=ord_val+32
-#     print(chr(conv_chr))
-
-val='ABC$TuuO'
+val= 'ABC'
 for x in val:
     ord_val=ord(x)
-    if ord_val in range(65,91):
-        print(chr(ord_val+32), end='')
-    elif ord_val in range(97,123):
-        print(chr(ord_val-32),end='')
-    else:
-        print(x,end='')
+    conv_chr=ord_val+32
+    print(chr(conv_chr))
+
+# val='ABC$TuuO'
+# for x in val:
+#     ord_val=ord(x)
+#     if ord_val in range(65,91):
+#         print(chr(ord_val+32), end='')
+#     elif ord_val in range(97,123):
+#         print(chr(ord_val-32),end='')
+#     else:
+#         print(x,end='')
