cf8fbc45cb142b45888fd8c2e38e8bf8b2530fc9
md sami <msami2241@gmail.com>
2024-12-26 09:05:23 +0530

    Update 24Dec.py

--- 24Dec.py
+++ 24Dec.py
@@ -1,25 +1,53 @@
 
 #>>>>>>>>>>>>>>>>>> QUIZ APPLications <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 
-corr_ans=0
-wrong_ans=0
+# corr_ans=0
+# wrong_ans=0
 
-print('1.what is the capital of india?')
-inp=input('enter ans:').lower()
-print(inp)
-if inp=='delhi':
-    corr_ans +=1
-    print('Correct asn')
-else:
-    wrong_ans +=1
-    print('wrong ans')
-print('2.what is capital of karnataka?')
-inp1=input('enter ans:').lower()
-print(inp1)
-if inp1=='bengaluru':
-    corr_ans +=1
-    print('correct ans')
-else:
-    wrong_ans +=1
-    print('wrong ans')
-print('percemtage',((corr_ans/(corr_ans+wrong_ans))*100))
+# print('1.what is the capital of india?')
+# inp=input('enter ans:').lower()
+# print(inp)
+# if inp=='delhi':
+#     corr_ans +=1
+#     print('Correct asn')
+# else:
+#     wrong_ans +=1
+#     print('wrong ans')
+# print('2.what is capital of karnataka?')
+# inp1=input('enter ans:').lower()
+# print(inp1)
+# if inp1=='bengaluru':
+#     corr_ans +=1
+#     print('correct ans')
+# else:
+#     wrong_ans +=1
+#     print('wrong ans')
+# print('percemtage',((corr_ans/(corr_ans+wrong_ans))*100))
+
+
+# >>>>>>>>>>>>>>>>>>>>QUIZ APP Reduced<<<<<<<<<<<<<<<<<<<<
+
+# corr_ans=0
+# wrong_ans=0
+# q=['1.what is the capital of india?',
+#    '2.what is the national animal of india?',
+#    '3.what is the capital of karnataka?',
+#    '4.what is the national bird of india?',
+#    '5.what is the natinal game of india?']
+# ans=['delhi','tiger','bengaluru','peacock','hockey']
+# for x in range(len(q)):
+#     print(q[x])
+#     user_ans=input('enter answer:').lower()
+#     if user_ans in ans[x]:
+#         corr_ans +=1
+#         print('correct ans')
+#     else:
+#         wrong_ans +=1
+#         print('wrong answer')
+# print('percentage',((corr_ans/(corr_ans+wrong_ans))*100))
+
+sales=[100,200,[60,70]]
+print(sales[2][1])
+        
+val=[1,44,66,[55,77],90]
+print(val[4])
