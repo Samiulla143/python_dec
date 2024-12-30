ed4999641e22cde9083dc3bd9c02ce7da6504f3e
md sami <msami2241@gmail.com>
2024-12-30 08:41:20 +0530

    Create 30Dec_small_highest.py

---
+++ 30Dec_small_highest.py
@@ -0,0 +1,17 @@
+# atoms=[1,66,4,56,88,99]
+# # print(atoms.sort())
+# # print(atoms[0]+ atoms[-1])
+# print(min(atoms))
+# print(max(atoms))
+# # print(atoms[0])
+
+
+val = [1,3,4,5,6,7]
+emp_list =[]
+for i in val:
+    if i%2==1:
+        emp_list.append(i)
+    else:
+        pass
+print(emp_list)
+        
