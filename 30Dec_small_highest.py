bc88efa03dc32bac9a86d7f107ce3963d4ac35e1
md sami <msami2241@gmail.com>
2024-12-30 11:54:54 +0530

    Update 30Dec_small_highest.py

--- 30Dec_small_highest.py
+++ 30Dec_small_highest.py
@@ -6,12 +6,19 @@
 # # print(atoms[0])
 
 
-val = [1,3,4,5,6,7]
-emp_list =[]
-for i in val:
-    if i%2==1:
-        emp_list.append(i)
-    else:
-        pass
-print(emp_list)
+# val = [1,3,4,5,6,7]
+# emp_list =[]
+# for i in val:
+#     if i%2==1:
+#         emp_list.append(i)
+#     else:
+#         pass
+# print(emp_list)
+
+#resuced form of above pgm
+print([x for x in [1,3,4,5,6,7] if x%2==1])
+
+print([f'{x} is even' if x%2==0 else f'{x} is odd' for x in range(1,9)])
+
+
         
