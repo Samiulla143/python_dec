8dd5a5aca8f78328200c018c3502b8d74ad44750
md sami <msami2241@gmail.com>
2025-01-01 09:44:48 +0530

    Update 31Dec_tickt_booking.py

--- 31Dec_tickt_booking.py
+++ 31Dec_tickt_booking.py
@@ -1,7 +1,36 @@
-tkts=[1,2,3,4,5]
-print('Available Tickets', tkts)
-user_inp = int(input('Enter quantity of tickets u wnanna buy:'))
-for x in range(user_inp):
-    ticket_num = int(input('enter ticket number:'))
-    tkts.remove(ticket_num)
-print('Available tkts', tkts)
+# tkts=[1,2,3,4,5]
+# print('Available Tickets', tkts)
+# user_inp = int(input('Enter quantity of tickets u wnanna buy:'))
+# for x in range(user_inp):
+#     ticket_num = int(input('enter ticket number:'))
+#     tkts.remove(ticket_num)
+# print('Available tkts', tkts)
+
+
+# >>>>>>>>>>>>>>>>>> SETS<<<<<<<<<<<<<<<<<<<<<<<
+
+# to get non duplicate values
+# lst=[1,2,3,3,4,5,5,6]
+# emp_list=[]
+# for x in lst:
+#     if x not in emp_list:
+#         emp_list.append(x)
+# print(emp_list)
+
+# compressed form
+
+# lst=[1,2,3,3,4,5,5,6]
+# emp_list=[]
+# [emp_list.append(x) for x in lst if x not in emp_list]
+# print(emp_list)
+
+# to get duplicate values
+lst=[1,2,3,3,4,5,5,6]
+emp_list=[]
+dup_list=[]
+for x in lst:
+    if x in emp_list:
+        dup_list.append(x)
+    else:
+        emp_list.append(x)
+print(dup_list)
