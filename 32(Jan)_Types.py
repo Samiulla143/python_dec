bcccd34e97f28ea6e3df239ade2801147801a734
md sami <msami2241@gmail.com>
2025-01-02 13:55:08 +0530

    Create 32(Jan)_Types.py

---
+++ 32(Jan)_Types.py
@@ -0,0 +1,42 @@
+# sales=[]
+# print(type(sales))
+
+# sales={}
+# print(type(sales))
+
+# sales={1}
+# print(type(sales))
+
+
+# val_2023={1,2,3}
+# val_2024={3,4,5}
+# print(val_2023.union(val_2024))
+# print(val_2023.intersection(val_2024))
+# print(val_2023.difference(val_2024))
+# print(val_2024.difference(val_2023))
+# print(val_2023.symmetric_difference(val_2024))
+
+sql={1,2,3,4,5}
+python={1,6,7,8}
+
+print(sql.intersection(python))
+print(sql.difference(python))
+print(sql.union(python))
+print(sql.intersection(python))
+print(python.difference(sql))
+print(python.symmetric_difference(sql))
+
+# sql={1,2,3,4,5}
+# python={1,6,7,8}
+# aws={1,4,5,6,99}
+
+# c3=print(sql.intersection(python))
+# print(c3.intersection(aws))
+
+# print(sql.symmetric_difference(python).symmetric_difference(aws))
+
+# print(python.difference(sql).difference(aws))
+
+# print(python.union(sql).union(aws))
+
+# print()
