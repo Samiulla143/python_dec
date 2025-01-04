310a166322ef16ce0ab51c547521a960d312e1dd
md sami <msami2241@gmail.com>
2025-01-04 21:56:31 +0530

    Create Assesment.py

---
+++ Assesment.py
@@ -0,0 +1,50 @@
+
+#1.Write a program that takes two numbers as input from the user and prints their sum.
+
+a=int(input('enter num1:'))
+b=int(input('enter num2:'))
+print(a+b)
+
+#.Write a program that takes the length and width of a rectangle as input from the user and calculates its area.
+
+a=int(input('Enter Length of Rectangle:'))
+b=int(input('Enter Width of Rectangle:'))
+print('Area is:',a*b)
+
+#3.Write a program to convert temperature from Celsius to Fahrenheit. The formula is `F = (C * 9/5) + 32`.
+
+a=int(input('Enter Temp in Celcius:'))
+print('Temp in fahrenheit is',(a*9/5)+32)
+
+
+
+#''' 4.Write a program that takes the principal amount, rate of interest, and time in years as input and
+#calculates the simple interest.'''
+a=int(input('Enter Principal Amount:'))
+b=int(input('Enter rate of Interest:'))
+c=int(input('Enter Time in Year:'))
+print('Simple Interest is',(a*b*c)/100)
+
+
+#5.Write a program to take three numbers as input from the user and calculate their average.
+a=int(input('enter num1:'))
+b=int(input('enter num2:'))
+c=int(input('enter num3:'))
+print('Average is',(a+b+c)/3)
+
+#'''6.Write a program that takes the radius of a circle as input from the user and calculates its perimeter
+#(circumference). The formula is `Perimeter = 2 * π * radius` (use 3.14159 for π).'''
+
+
+#7.Write a program that takes an integer input from the user and checks whether it is even or odd.
+
+
+
+#8.Write a program that takes two numbers as input and swaps their values.
+
+
+#9.Write a program to take two numbers from the user and print their product.
+
+
+#10.Write a program that takes a time in minutes from the user and converts it to seconds.
+
