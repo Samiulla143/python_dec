15f95d498681e948160a0db8cf0f26275f08915d
md sami <msami2241@gmail.com>
2025-01-07 09:07:20 +0530

    Create 33_jan(07).py

---
+++ 33_jan(07).py
@@ -0,0 +1,55 @@
+# lst=[10,20,30,40,50]
+# print(lst)
+# lst[1:4]=99
+# print(lst)
+
+# lst=[10,20,30,40,50]
+# print(lst)
+# lst[1:4]=[99]
+# print(lst)
+
+# lst=[10,20,30,40,50]
+# print(lst)
+# lst[1:4]=[99,88,77]
+# print(lst)
+
+# lst=[10,20,30,40,50]
+# lst.remove(30)
+# print(lst)
+
+# lst=[10,20,30,40,50]*2   #duplication
+# print(lst)
+
+# lst=[10,20,30,40,50]*2 
+# print(30 in lst)
+# print(30 not in lst)
+
+# a=[10,10,20]
+# for x in set(a):
+#     print(x,'-->', a.count(x))
+
+# a='armani'
+# vowels='aeiou'
+# for x in a:
+#     if x in vowels:
+#         print(x)
+        
+# # using not in         
+# a='armani'
+# vowels='aeiou'
+# for x in a:
+#     if x not in vowels:
+#         print(x,end='')
+ 
+#pop is used to remove the value       
+# lst=[10,20,30,40,50,60]
+# print(lst)
+# lst.pop()
+# print(lst)
+
+lst=[10,20,30,40,50]
+print(lst)
+lst.pop(3)
+print(lst)
+
+
