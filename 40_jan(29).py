c5a61069eb4299e7c280fd6c715f60bc72fefe0b
md sami <msami2241@gmail.com>
2025-01-29 12:58:27 +0530

    Update 40_jan(29).py

--- 40_jan(29).py
+++ 40_jan(29).py
@@ -9,13 +9,22 @@ class passenger(plane):
 class cargo(plane):
     def carry_goods(self):
         print('plane carries goods')
+class fighter(plane):
+    def carry_weapons(self):
+        print('carry weapons')
 
 p=passenger()
 p.land()
 p.takeoff()
 p.carry_passenger()
+
 c=cargo()
 c.land()
 c.takeoff()
 c.carry_goods()
+
+f=fighter()
+f.land()
+f.takeoff()
+f.carry_weapons()
 
